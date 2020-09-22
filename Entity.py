55b3294e61c81a7286a44a94d03df93a33d2fab2
naberatu <naberatu@gmail.com>
2020-09-21 22:30:25 -0700

    Starting to make basis for Entity file. Uploaded example images.

---
+++ Entity.py
@@ -0,0 +1,32 @@
+import base64
+from PIL import Image
+import io
+
+class Entity():
+
+    def __init__(self, name, weight, tileSize, stackSize):
+        self.name = name
+        self.weight = weight
+        self.tileSize = tileSize
+        self.stackSize = stackSize
+
+        with open("")
+        thumbnail = Image
+
+    def getName(self):
+        return self.name
+
+    def getImage(self):
+        return self.
+
+    def getWeight(self):
+        return self.weight
+
+    def getTileSize(self):
+        return self.tileSize
+
+    def getStackSize(self):
+        return self.stackSize
+
+
+
