83ad76e67e919ff45e1294700fffea12d0d38054
naberatu <naberatu@gmail.com>
2020-09-27 13:55:52 -0700

    Updated class names to better fit conventions. Started Animate Class.

---
+++ animate.py
@@ -0,0 +1,8 @@
+
+from entity import Entity
+class Animate(Entity):
+
+    def __init__(self, name, weight=None, tile_size=None, stack_size=None):
+        super(Animate, self).__init__(name, weight=None, tile_size=None, stack_size=None)
+
+        #don't think I translated this correctly???
