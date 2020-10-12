8011d054b244b9b7b127ff19e341f5798d2c2411
naberatu <naberatu@gmail.com>
2020-10-11 18:39:57 -0700

    Created player. Slightly modified animate & inanimate.

--- inanimate.py
+++ inanimate.py
@@ -3,10 +3,14 @@ from entity import Entity
 
 
 class Inanimate(Entity):
-
     def __init__(self, name, id, isItem, max_stack=None):
         super().__init__(name, id)    # should inherit everything this way
         self.isItem = isItem
+
+        if isItem:
+            self.itemType = "item"
+        else:
+            self.itemType = "prop"
 
         if max_stack is not None:
             self.maxStack = max_stack
@@ -16,8 +20,19 @@ class Inanimate(Entity):
         self.tileSize = 1
         self.visible = False
 
+    # Accessors
+    # ==================================
     def set_stack_size(self, size):
         self.maxStack = size
 
+    def get_type(self):
+        return self.itemType
+
+    # Mutators
+    # ==================================
     def get_stack_size(self):
         return self.maxStack
+
+    def mod_type(self, item_type):
+        self.itemType = item_type
+
---
+++ player.py
@@ -0,0 +1,54 @@
+
+from animate import Animate
+from statblock import StatBlock
+
+
+class Player(Animate):
+    def __init__(self, name, id, role, race, gender, level):
+        super().__init__(name, id)      # should inherit everything this way
+        self.statBlock = StatBlock()    # empty stat block
+        self.role1 = role               # Distinct just in case we add multi-classing.
+        self.role2 = None               # We can adjust this later on
+        self.race = race
+        self.gender = gender
+        self.level = level
+        self.exp = 0                    # This should be changed depending on the player's level?
+        self.maxInvWeight = 200         # Arbitrary value
+        self.companion = None           # We can add functionality to adjust later
+
+    # Accessors
+    # ==================================
+    def get_level(self):
+        return self.level
+
+    def get_exp(self):
+        return self.exp
+
+    def get_role(self):
+        if self.role2 is not None:
+            return self.role1, self.role2
+        else:
+            return self.role1
+
+    def get_race(self):
+        return self.race
+
+    # Mutators
+    # ==================================
+    def level_up(self):
+        self.level += 1
+
+    def mod_level(self, value):
+        self.level = value
+
+    def gain_exp(self, amount):
+        self.exp += amount
+
+    def mod_role(self, role):
+        self.role1 = role
+
+    def mod_race(self, race):
+        self.race = race
+
+    def add_multiclass(self, multiclass):
+        self.role2 = multiclass
