6d8d66d63a834dfe5f9d780bc07b5ec939aee896
naberatu <naberatu@gmail.com>
2020-10-11 19:13:54 -0700

    Created Non-Commbat-Character (NCC) class.

---
+++ ncc.py
@@ -0,0 +1,29 @@
+
+from animate import Animate
+from statblock import StatBlock
+
+
+class NCC(Animate):
+    def __init__(self, name, id, role, race):
+        super().__init__(name, id)      # should inherit everything this way
+        self.statBlock = StatBlock()    # empty stat block
+        self.role = role               # Distinct just in case we add multi-classing.
+        self.race = race
+        # self.gender = gender
+        self.maxInvSize = 20
+
+    # Accessors
+    # ==================================
+    def get_role(self):
+        return self.role
+
+    def get_race(self):
+        return self.race
+
+    # Mutators
+    # ==================================
+    def mod_role(self, role):
+        self.role = role
+
+    def mod_race(self, race):
+        self.race = race
