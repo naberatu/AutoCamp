127f59e48626c6b106bd0331837b72fb1989ea46
naberatu <naberatu@gmail.com>
2020-10-11 18:13:56 -0700

    Slightly updated inanimate.py

--- inanimate.py
+++ inanimate.py
@@ -1,18 +1,20 @@
 
 from entity import Entity
 
+
 class Inanimate(Entity):
 
-    def __init__(self, name, id, isItem, maxStack=None):
+    def __init__(self, name, id, isItem, max_stack=None):
         super().__init__(name, id)    # should inherit everything this way
         self.isItem = isItem
 
-        if maxStack is not None:
-            self.maxStack = maxStack
+        if max_stack is not None:
+            self.maxStack = max_stack
         else:
             self.maxStack = 1
 
         self.tileSize = 1
+        self.visible = False
 
     def set_stack_size(self, size):
         self.maxStack = size
