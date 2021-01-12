5d77d6cee8e4688eedafa6cf410902b427d47b3b
abcdjosette <abcdjosette@gmail.com>
2021-01-12 14:07:27 -0800

    Start + Quit Button added :D

--- display.py
+++ display.py
@@ -10,6 +10,9 @@ class Display:
     black = (0, 0, 0)
     white = (255, 255, 255)
     red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+    click = False
 
     gameDisplay = pygame.display.set_mode((display_width, display_height))
     pygame.display.set_caption('AutoCamp32')
@@ -21,18 +24,46 @@ class Display:
 
     def game_intro(self):
         intro = True
+        start_button = pygame.Rect(100, 330, 100, 50)
+        quit_button = pygame.Rect(600, 330, 100, 50)
 
         while intro:
-            for event in pygame.event.get():
-                print(event)
-                if event.type == pygame.QUIT:
-                    intro = False
 
             self.gameDisplay.fill(self.white)
+
             largeText = pygame.font.Font('freesansbold.ttf', 60)
             TextSurf, TextRect = self.text_objects("Welcome to AutoCamp!", largeText)
             TextRect.center = ((self.display_width / 2), (self.display_height / 2))
+
+            pygame.draw.rect(self.gameDisplay, (self.blue), start_button)
+            StartText = pygame.font.Font('freesansbold.ttf', 20)
+            StartSurf, StartRect = self.text_objects("Start!!", StartText)
+            StartRect.center = (150, 355)
+
+            pygame.draw.rect(self.gameDisplay, (self.red), quit_button)
+            QuitText = pygame.font.Font('freesansbold.ttf', 20)
+            QuitSurf, QuitRect = self.text_objects("Leave? D:", QuitText)
+            QuitRect.center = (650, 355)
+
+            mx, my = pygame.mouse.get_pos()
+            if start_button.collidepoint((mx, my)):
+                if self.click:
+                    intro = False
+            if quit_button.collidepoint((mx, my)):
+                if self.click:
+                    intro = False
+
+            self.click = False
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.click = True
+
             self.gameDisplay.blit(TextSurf, TextRect)
+            self.gameDisplay.blit(StartSurf, StartRect)
+            self.gameDisplay.blit(QuitSurf, QuitRect)
             pygame.display.update()
             self.clock.tick(15)
 
