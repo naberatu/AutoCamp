2d7a6ac67068960401001cfe4c02dd18207ca016
naberatu <naberatu@gmail.com>
2021-02-20 16:22:17 -0800

    changed run style

--- master_main.py
+++ master_main.py
@@ -1,4 +1,13 @@
-#!/usr/bin/python
+# /etc/init.d/sample.py
+### BEGIN INIT INFO
+# Provides:          master_main.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
 
 
 from statblock import StatBlock
